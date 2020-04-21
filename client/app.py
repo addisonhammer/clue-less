909769e130fd486cafc04e14d5861feae2361f08
alanaalfeche <alanaalfeche@gmail.com>
2020-04-21 16:43:17 -0500

    Draft home and queue UI components

--- client/app.py
+++ client/app.py
@@ -1,19 +1,13 @@
-"""Example Client that generates a random accusation and sends it to the server."""
 import json
 import logging
 import os
-import random
-import socket
-import time
 
-from flask import Flask, request, render_template, jsonify
-import requests
+from flask import Flask, request, render_template, jsonify, redirect, url_for
 
 from core.messages import PlayerMoveRequest, PlayerMoveResponse
 from core.server_boundary import Server
 
-# from core import game_rules
-# from core import messages
+from core import game_const
 
 SERVER_IP = os.environ.get('SERVER_IP')
 SERVER_PORT = os.environ.get('SERVER_PORT')
@@ -28,16 +22,37 @@ class App(Flask):
 APP = App(__name__)
 
 
-@APP.route('/join', methods=['GET'])
+@APP.route('/join', methods=['GET', 'POST'])
 def join_game():
-    if request.method == 'GET':
-        logging.info('GET call, serving HOME template')
-        return render_template('home.html.jinja')
-    logging.info('Form Results: %s', request.form)
-    player_selection = request.form.get('player')
-    if player_selection:  # Add more error checking here
-        result = APP.server.send_join_request(player_selection)
-    return jsonify(result)
+    if request.method == 'POST':
+        character_selection = request.form.get('character')
+        logging.info('Selected character: %s', character_selection)
+        if character_selection:
+            result = APP.server.send_join_request(character_selection)
+            logging.info(result)
+            # result does not return client_id
+        return redirect(url_for('queue', client_id=3))
+
+    return render_template('home.html.jinja',
+                           characters=list(game_const.CHARACTERS))
+
+
+@APP.route('/queue/<int:client_id>', methods=['GET', 'POST'])
+def queue(client_id):
+    if request.method == 'POST':
+        if "start_game" in request.form:
+            logging.info('Client %s request to start the game', client_id)
+            # result = APP.server.send_start_game_request()
+            # logging.info(result)
+            # TypeError: send_start_game_request() missing 1 required positional argument: 'game_id'
+        return redirect(url_for('game', client_id=3, game_id=3))
+
+    return render_template('queue.html.jinja')
+
+
+@APP.route('/game/<int:game_id>/<int:client_id>', methods=['GET', 'POST'])
+def game(client_id, game_id):
+    return render_template('clue.html.jinja')
 
 
 @APP.route('/api/player_move', methods=['GET'])
@@ -52,6 +67,6 @@ def player_move():
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    APP.config.update(PROPAGATE_EXCEPTIONS=True)
+    # APP.config.update(PROPAGATE_EXCEPTIONS=True)
     APP.run(debug=True, host='0.0.0.0')
     APP.secret_key == u'yolo'
