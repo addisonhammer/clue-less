14bcee316553aa9ebac30ef3cf81bbd41d6322a3
alanaalfeche <alanaalfeche@gmail.com>
2020-04-27 21:23:39 -0500

    Add dockerized react application

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Clue-less Application
+      </header>
+    </div>
+  );
+}
+
+export default App;
