8c88599464909e582e5717efb77b626278e8aef8
alanaalfeche <alanaalfeche@gmail.com>
2020-05-02 21:44:41 -0500

    Try to make connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          Clue-less Application
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+    fetch('http://localhost:5000/api/join_game', {
+      mode: "no-cors",
+      method: "POST",
+      body: {'player': 'alana'}
+    })
+    .then(res => console.log(res))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={this.handleClick}>
+          Join Game
+          </button>
+        </header>
+      </div>
+    );
+  
+  }
 }
 
 export default App;
